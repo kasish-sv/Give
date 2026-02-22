b7a73dcfe39f3adae46af7362d1e89bd7853d35d
kasish-sv <kasishshanmugam1@gmail.com>
2026-02-22 19:23:05 +0530

    Update FormSubmit.tsx

--- components/form/FormSubmit.tsx
+++ components/form/FormSubmit.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
+import { Spinner } from "../ui/spinner";
 
 export const FormSubmit = ({ value }: { value: string }) => {
   const { pending } = useFormStatus();
   return (
-    <Button
-      type="submit"
-      className="disabled:bg-gray-600 w-full text-xs md:text-sm"
-      disabled={pending}
-    >
-      {value}
-    </Button>
+    <div>
+      <Button
+        type="submit"
+        className="disabled:bg-gray-600 w-full text-xs md:text-sm"
+        disabled={pending}
+      >
+        {value}
+        {pending && <Spinner className="m-2" />}
+      </Button>
+    </div>
   );
 };
